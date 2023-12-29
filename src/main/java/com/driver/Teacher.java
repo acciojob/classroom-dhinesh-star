174b21cf5ecad9d6566f90a2a740dd5be632e819
dhinesh-star <dhineshmanickam2001@gmail.com>
2023-12-30 02:11:36 +0530

    Submission3

--- src/main/java/com/driver/Teacher.java
+++ src/main/java/com/driver/Teacher.java
@@ -14,7 +14,7 @@ public class Teacher {
 
     public Teacher(String name, int numberOfStudents, int age) {
         this.name = name;
-        this.numberOfStudents = 0;
+        this.numberOfStudents = numberOfStudents;
         this.age = age;
     }
 
